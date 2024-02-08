3d3bf5a823c4275b65f1447fa171ed5c16b48bcf
SHYAM <SMUSANI@UCO.EDU>
2024-02-07 23:19:59 -0600

    2nd changes

--- chapter/js03_txt.js
+++ chapter/js03_txt.js
@@ -9,4 +9,54 @@
 
      Filename: js03.js
  */
+let weekDays = ["Sunday","Monday", "Tuesday", "Wednesday","Thursday","Friday","Saturday"];
 
+
+
+window.addEventListener("load", addWeekDays);
+function addWeekDays(){
+    let i = 0;
+    let headingcells =  document.getElementsByTagName("th")
+    while(i < 7) {
+        headingcells[i].innerHTML = weekDays[i];
+        i++;
+    }
+}
+
+window.addEventListener("load", showGames);
+
+function showGames() {
+    for(let i = 0; i < gameDates.length; i++) {
+        let gameInfo = "";
+        switch(gameResults[i]) {
+        case "W":
+            gameInfo += "<p class='win'>";
+            break;
+        
+        case "L":
+            gameInfo += "<p class='lose'>";
+            break;
+
+        case "S":
+            gameInfo += "<p class='suspended'>";
+            break;
+        
+        case "P":
+            gameInfo += "<p class='postponed'>";
+            break;
+            
+         }
+
+        
+
+
+        gameInfo += gameOpponents[i] + "<br>";
+        
+        gameInfo += gameResults[i] + ": (" + runsScored[i] + "-" + runsAllowed[i] + ")" ;
+
+        gameInfo += "</p>";
+
+        let tableCell = document.getElementById(gameDates[i]);
+        tableCell.insertAdjacentHTML("beforeEnd", gameInfo);
+    }
+}
